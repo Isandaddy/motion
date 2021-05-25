99bab8950cf947dd881e50cb3951c32f3453ac43
Seung Tack Jo <jstack87215@gmail.com>
2021-05-25 14:57:17 +0900

    refactoring-gv : PageComponent DI

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -6,7 +6,15 @@ export interface Composable {
 
 type OnCloseListener = () => void;
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+interface SectionContainer extends Component, Composable {
+    setOnCloseListner (listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+    new (): SectionContainer;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
     private closeListner?: OnCloseListener;
     
     constructor(){
@@ -18,8 +26,9 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
                 </li>`);
         const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;   
             closeBtn.onclick = () => {
+                console.log(this.element);
                 this.closeListner && this.closeListner();
-            }
+            };
     }
     
     addChild(child: Component){
@@ -32,12 +41,13 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
     }
 }
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-    constructor() {
+    constructor(private pageItemConstructor: SectionContainerConstructor) {
         super('<ul class="page"></ul>');
     }
     addChild(section: Component) {
-        const item = new PageItemComponent();
+        const item = new this.pageItemConstructor();
         item.addChild(section);
+        
         item.attachTo(this.element, 'beforeend');
         item.setOnCloseListner(() => {
             item.removeFrom(this.element);
